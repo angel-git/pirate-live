867e78724837f55398fd3645bb8f4cac4a7bdbc7
Angel Gavalda <angel.gavalda@gmail.com>
2014-11-25 21:56:28 +0100

    added chrome extension info

--- coffee/home.coffee
+++ coffee/home.coffee
@@ -1,8 +1,11 @@
 calendarContent = ''
 
 angular.module('home', [])
-.controller 'HomeController', ($scope, $http) ->
-  searchSeries(new Date(), $http)
+  .controller 'HomeController', ($scope, $http) ->
+    searchSeries(new Date(), $http)
+
+
+
 
 
 searchSeries = (dateToSeach, $http) ->
@@ -13,9 +16,9 @@ getSeriesDate = (dateToSearch, $http, success) ->
   month = dateToSearch.getMonth() + 1
   todayFormat = '#d_' + dateToSearch.getDate() + '_' + month + '_' + dateToSearch.getFullYear()
   if calendarContent is ''
-    $http.get 'http://www.pogdesign.co.uk/cat', (error, response, body) ->
-      if response.statusCode == 200
-        calendarContent = body
+    $http.get('http://www.pogdesign.co.uk/cat/').success (data, status, headers, config) ->
+      if status = 200
+        calendarContent = data
         parseCalendar(todayFormat, success)
       else
         alert('something went wrong with the calendar connection: ' + response.statusCode)
@@ -23,7 +26,7 @@ getSeriesDate = (dateToSearch, $http, success) ->
   else
     parseCalendar(todayFormat, success)
 
-@parseCalendar = (todayFormat, success) ->
+parseCalendar = (todayFormat, success) ->
   divs = $(calendarContent).find(todayFormat).find('div');
   serieList = []
   for div in divs
@@ -36,3 +39,25 @@ getSeriesDate = (dateToSearch, $http, success) ->
     serie = new Serie(serieTitleString, serieEpisodeString, className)
     serieList.push(serie)
   success? serieList
+
+parseEpisode = (epInput) ->
+  episode = ''
+  splitted = epInput.split(' ')
+  season = splitted[1]
+  ep = splitted[4]
+  if season.length < 2
+    episode += 's0' + season
+  else
+    episode += 's' + season
+  if ep.length < 2
+    episode += 'e0' + ep
+  else
+    episode += 'e' + ep
+  return episode
+
+
+class Serie
+  constructor: (@serie, @episode, @className) ->
+
+class Torrent
+  constructor: (@name, @leeds, @seeds, @link, @desc) ->
--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 (function() {
-  var calendarContent, getSeriesDate, searchSeries;
+  var Serie, Torrent, calendarContent, getSeriesDate, parseCalendar, parseEpisode, searchSeries;
 
   calendarContent = '';
 
@@ -18,9 +18,9 @@
     month = dateToSearch.getMonth() + 1;
     todayFormat = '#d_' + dateToSearch.getDate() + '_' + month + '_' + dateToSearch.getFullYear();
     if (calendarContent === '') {
-      return $http.get('http://www.pogdesign.co.uk/cat', function(error, response, body) {
-        if (response.statusCode === 200) {
-          calendarContent = body;
+      return $http.get('http://www.pogdesign.co.uk/cat/').success(function(data, status, headers, config) {
+        if (status = 200) {
+          calendarContent = data;
           return parseCalendar(todayFormat, success);
         } else {
           alert('something went wrong with the calendar connection: ' + response.statusCode);
@@ -32,7 +32,7 @@
     }
   };
 
-  this.parseCalendar = function(todayFormat, success) {
+  parseCalendar = function(todayFormat, success) {
     var as, className, div, divs, jdiv, serie, serieEpisode, serieEpisodeString, serieList, serieTitleString, _i, _len;
     divs = $(calendarContent).find(todayFormat).find('div');
     serieList = [];
@@ -49,5 +49,48 @@
     }
     return typeof success === "function" ? success(serieList) : void 0;
   };
+
+  parseEpisode = function(epInput) {
+    var ep, episode, season, splitted;
+    episode = '';
+    splitted = epInput.split(' ');
+    season = splitted[1];
+    ep = splitted[4];
+    if (season.length < 2) {
+      episode += 's0' + season;
+    } else {
+      episode += 's' + season;
+    }
+    if (ep.length < 2) {
+      episode += 'e0' + ep;
+    } else {
+      episode += 'e' + ep;
+    }
+    return episode;
+  };
+
+  Serie = (function() {
+    function Serie(serie, episode, className) {
+      this.serie = serie;
+      this.episode = episode;
+      this.className = className;
+    }
+
+    return Serie;
+
+  })();
+
+  Torrent = (function() {
+    function Torrent(name, leeds, seeds, link, desc) {
+      this.name = name;
+      this.leeds = leeds;
+      this.seeds = seeds;
+      this.link = link;
+      this.desc = desc;
+    }
+
+    return Torrent;
+
+  })();
 
 }).call(this);
